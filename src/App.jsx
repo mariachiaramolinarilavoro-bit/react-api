1530687aa5c22731b2fce64a0b668d38325a4607
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-03-22 16:09:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AppFooter from './components/AppFooter';
+import AppHeader from './components/AppHeader.jsx';
+import AppMain from './components/AppMain';
+
+function App() {
+
+  return (
+    <>
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,6 @@
+export default function AppFooter() {
+
+    return (
+        <div>Hi</div>
+    )
+}
