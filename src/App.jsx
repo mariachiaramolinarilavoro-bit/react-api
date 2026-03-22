3adad379d736d4310a2a98a696bb0dc7f03e36a5
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-03-22 16:50:05 +0100

    milestone 2 complete,add basic structure for cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import AppHeader from './components/AppHeader.jsx';
 import AppMain from './components/AppMain';
 
 function App() {
+  const [actors, setActors] = useState([]);
 
   useEffect(() => {
     fetch('https://lanciweb.github.io/demo/api/actors/')
       .then(res => res.json())
       .then(data => {
         console.log(data);
+        setActors(data);
       })
   }, []);
 
   return (
     <>
       <AppHeader />
-      <AppMain />
+      <AppMain actors={actors} />
       <AppFooter />
     </>
   )
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,6 +1,17 @@
-export default function AppMain() {
+export default function AppMain({ actors }) {
 
     return (
-        <div>Hi</div>
+        <main>
+            {actors.map(actor => (
+                <div key={actor.id}>
+                    <img src={actor.image} alt={actor.name} />
+                    <h2>{actor.name}</h2>
+                    <p>{actor.birth_year}</p>
+                    <p>{actor.nationality}</p>
+                    <p>{actor.biography}</p>
+                    <p>{actor.awards}</p>
+                </div>
+            ))}
+        </main>
     )
 }
