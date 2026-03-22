376b9007a2383e3f34ced0e36f6269d89ccc725e
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-03-22 16:20:47 +0100

    milestone 1 complete, add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState, useEffect } from 'react';
 import AppFooter from './components/AppFooter';
 import AppHeader from './components/AppHeader.jsx';
 import AppMain from './components/AppMain';
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://lanciweb.github.io/demo/api/actors/')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+  }, []);
 
   return (
     <>
